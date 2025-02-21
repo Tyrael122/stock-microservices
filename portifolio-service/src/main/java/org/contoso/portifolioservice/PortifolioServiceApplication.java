edce09434c9086a0273f213c2f4c47fc3afeb842
tyrael <kanbme@protonmail.com>
2025-02-21 13:16:54 -0300

    Creating portfolio service

---
+++ portifolio-service/src/main/java/org/contoso/portifolioservice/PortifolioServiceApplication.java
@@ -0,0 +1,13 @@
+package org.contoso.portifolioservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PortifolioServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PortifolioServiceApplication.class, args);
+	}
+
+}
---
+++ portifolio-service/src/test/java/org/contoso/portifolioservice/PortifolioServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package org.contoso.portifolioservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PortifolioServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
